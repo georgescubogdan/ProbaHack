90771d319b0b6da46b13bc199bb65bfeebb23122
Lupea Mihai <mihai.lupea@gmail.com>
2019-03-08 23:07:33 +0200

    Merge commit 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { MaterialContainerModule } from './material-container/material-container.module';
-=======
-import { CompartmentComponent } from './compartment/compartment.component';
->>>>>>> 7758eb1ed915bd4b8fc8d402bd4e183725281307
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CompartmentComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
