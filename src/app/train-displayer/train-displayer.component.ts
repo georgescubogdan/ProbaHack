5f86524fd3660810735a0afcc3821ae07947445a
Lupea Mihai <mihai.lupea@gmail.com>
2019-03-10 22:36:17 +0200

    Sorted carts and compartments for train view

--- src/app/train-displayer/train-displayer.component.ts
+++ src/app/train-displayer/train-displayer.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { DataProviderService } from '../data-provider.service';
 import { Station } from '../station';
+import { Cart } from '../cart';
 @Component({
   selector: 'app-train-displayer',
   templateUrl: './train-displayer.component.html',
@@ -10,6 +11,15 @@ export class TrainDisplayerComponent implements OnInit {
   stations: Station[];
   @Input() stationNumber: number;
   constructor(private dataProvider: DataProviderService) { }
+
+  getSortedCarts(station: Station) {
+    return station.carts.sort((a, b) => a.cartNumber - b.cartNumber);
+  }
+
+  getSortedCompartments(cart: Cart){
+    return cart.compartments.sort((a, b) => a.compartmentNumber - b.compartmentNumber);
+  }
+
 
   ngOnInit() {
     this.stations = this.dataProvider.stations;
